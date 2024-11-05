2e5647865d9e803e2534764990a43d10b777d2e9
jess4587 <jesus.spraus@gmail.com>
2024-11-05 10:52:46 -0400

    actualizado

--- diccionario.py
+++ diccionario.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data= json.load(open("palabras.json"))
 
@@ -6,14 +7,33 @@ def significado(x):
 
     if x in data:
         return data[x]
-
+    
+    elif len(get_close_matches(x, data.keys())) > 0: 
+        
+        concidencia= get_close_matches(x, data.keys()) [0]
+    
+        print("Quieres decir:", concidencia)
+        print("Si es correctar presiona (Y) si no presiona (N) ")
+        respuesta = input()
+    
+        if respuesta.lower() == "y":
+            return(data[concidencia])
+        
+        elif respuesta.lower()== "n":
+            return("No se que palabra es quieres buscar")
+        
+        
     else:
         return("La palabra no existe,ingrese otra palabra")
 
 
-palabra= input("Ingrese una palabra en ingles porque estamos corto de idiomas:\n").lower()
+palabra= input("Ingrese una palabra :\n").lower()
 
-print(significado(palabra))
+k= significado(palabra)
 
-
+if type(k) == list:
+    for item in k:
+         print(item)
+else:
+    print(k)    
   
